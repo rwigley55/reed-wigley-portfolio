eb785c989c1c7f2130922fc05e37af5cb185768e
Reed Wigley <rwigley55@gmail.com>
2023-08-03 10:12:05 -0700

    Mobile nav clean up

--- src/components/SiteNav.js
+++ src/components/SiteNav.js
@@ -6,31 +6,45 @@ import styles from "./SiteNav.module.css";
 
 const SiteNav = (props) => {
   return (
-    <Navbar expand="lg" className="bg-secondary p-0 sticky-top border-bottom border-3 border-primary">
-        <Navbar.Toggle className="ms-auto p-1 mt-1 mb-1 b-0" aria-controls="navbar-nav" />
-        <Navbar.Collapse style={{color: 'yellow'}} className="" id="navbar-nav">
-          <Nav className="ms-auto p-0 b-0">
-            <Nav.Link className={`${styles.small} text-primary text-lowercase p-1 ms-auto`} href="#home">
-              Home
-            </Nav.Link>
-            <Nav.Link className={`${styles.small} text-primary text-lowercase p-1 ms-auto`} href="#about">
-              About
-            </Nav.Link>
-            <Nav.Link className={`${styles.small} text-primary text-lowercase p-1 ms-auto`} href="#portfolio">
-              Portfolio
-            </Nav.Link>
-            <Nav.Link className={`${styles.small} text-primary text-lowercase p-1 ms-auto`} href="#contact">
-              Contact
-            </Nav.Link>
-            <Nav.Link
-              className={`${styles.small} text-lowercase text-secondary-emphasis p-1 ms-auto`}
-              href="#link"
-            >
-              Resume
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-
+    <Navbar
+      expand="lg"
+      className="bg-secondary p-0 sticky-top border-bottom border-3 border-primary"
+    >
+      <Navbar.Toggle className="ms-auto p-1 b-0" aria-controls="navbar-nav" />
+      <Navbar.Collapse className="" id="navbar-nav">
+        <Nav className="ms-auto p-0 b-0">
+          <Nav.Link
+            className={`${styles.small} text-primary text-lowercase p-1 ms-auto`}
+            href="#home"
+          >
+            Home
+          </Nav.Link>
+          <Nav.Link
+            className={`${styles.small} text-primary text-lowercase p-1 ms-auto`}
+            href="#about"
+          >
+            About
+          </Nav.Link>
+          <Nav.Link
+            className={`${styles.small} text-primary text-lowercase p-1 ms-auto`}
+            href="#portfolio"
+          >
+            Portfolio
+          </Nav.Link>
+          <Nav.Link
+            className={`${styles.small} text-primary text-lowercase p-1 ms-auto`}
+            href="#contact"
+          >
+            Contact
+          </Nav.Link>
+          <Nav.Link
+            className={`${styles.small} text-lowercase text-secondary-emphasis p-1 ms-auto`}
+            href="#link"
+          >
+            Resume
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
