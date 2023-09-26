48fb0494544264813e9b9ac8c1eb39db7daff98a
Reed Wigley <rwigley55@gmail.com>
2023-09-26 11:51:26 -0700

    Style project cards

--- modal.js
+++ modal.js
@@ -1,19 +1,56 @@
-// JavaScript to control modal visibility
-const openModalButton = document.getElementById("openModalBtn");
-const closeModalButton = document.getElementById("closeModalBtn");
-const modalOverlay = document.getElementById("modalOverlay");
+// Modal 1
+const openModal1 = document.getElementById("openModal1");
+const closeModalButton1 = document.getElementById("closeModalBtn1");
+const modalOverlay1 = document.getElementById("modalOverlay1");
 
-openModalButton.addEventListener("click", () => {
-  modalOverlay.style.display = "block";
+openModal1.addEventListener("click", () => {
+  modalOverlay1.style.display = "flex";
 });
 
-closeModalButton.addEventListener("click", () => {
-  modalOverlay.style.display = "none";
+closeModalButton1.addEventListener("click", () => {
+  modalOverlay1.style.display = "none";
 });
 
-// Close the modal when clicking outside of it
-modalOverlay.addEventListener("click", (event) => {
-  if (event.target === modalOverlay) {
-    modalOverlay.style.display = "none";
+modalOverlay1.addEventListener("click", (event) => {
+  if (event.target === modalOverlay1) {
+    modalOverlay1.style.display = "none";
+  }
+});
+
+// Modal 2
+const openModal2 = document.getElementById("openModal2");
+const closeModalButton2 = document.getElementById("closeModalBtn2");
+const modalOverlay2 = document.getElementById("modalOverlay2");
+
+openModal2.addEventListener("click", () => {
+  modalOverlay2.style.display = "flex";
+});
+
+closeModalButton2.addEventListener("click", () => {
+  modalOverlay2.style.display = "none";
+});
+
+modalOverlay2.addEventListener("click", (event) => {
+  if (event.target === modalOverlay2) {
+    modalOverlay2.style.display = "none";
+  }
+});
+
+// Modal 3
+const openModal3 = document.getElementById("openModal3");
+const closeModalButton3 = document.getElementById("closeModalBtn3");
+const modalOverlay3 = document.getElementById("modalOverlay3");
+
+openModal3.addEventListener("click", () => {
+  modalOverlay3.style.display = "flex";
+});
+
+closeModalButton3.addEventListener("click", () => {
+  modalOverlay3.style.display = "none";
+});
+
+modalOverlay3.addEventListener("click", (event) => {
+  if (event.target === modalOverlay3) {
+    modalOverlay3.style.display = "none";
   }
 });
